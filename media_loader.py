472247a6f3b8cfecc0fd2cf9b3a37e876fef340f
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-04-15 22:26:23 +0200

    Add standardise_columns function with type and source handling

--- media_loader.py
+++ media_loader.py
@@ -8,6 +8,42 @@ import sys
 import argparse
 import pandas as pd
 
+
+def load_csv(filepath):
+    """ Load csv file including error handling """
+    try:
+        return pd.read_csv(filepath)
+    except FileNotFoundError:
+        print(f"File not found: {filepath}")
+        sys.exit()
+
+
+def standardise_columns(df, source):
+    """ Create the standard column names for the Master CSV """
+    df_renamed = df.rename(columns={
+        'Const': 'imdb_id',
+        'Title': 'title',
+        'Year': 'year',
+        'Genres': 'genres',
+        'IMDb Rating': 'imdb_rating',
+        'Runtime (mins)': 'runtime_mins',
+        'Title Type': 'type',
+        'duration': 'runtime_mins'
+    })
+    
+    df_renamed['source'] = source # add source column e.g. plex_movies, plex_tv, imdb_movies, imdb_tv, local_movies, local_tv
+    # print(df_renamed['type'])
+    # print(source)
+    if 'tv' in source.lower():
+        df_renamed['type'] = 'tv_show'
+    
+    if 'movie' in source.lower():
+        df_renamed['type'] = 'movie'
+
+    return df_renamed
+
+
+# main logic
 parser = argparse.ArgumentParser(description='The program creates a master csv file from different sources.')
 parser.add_argument('--plex-movies', help='add the path of the exported (movie) file from plex')
 parser.add_argument('--plex-tv', help='add the path of the exported (tv show) file from plex')
@@ -16,14 +52,6 @@ parser.add_argument('--imdb-tv', help='add the path of the exported (tv show) fi
 parser.add_argument('--local-movies', help='add the path of the exported (movie) file from local scan')
 parser.add_argument('--local-tv', help='add the path of the exported (tv show) file from local scan')
 args = parser.parse_args()
-
-
-def load_csv(filepath):
-    try:
-        return pd.read_csv(filepath)
-    except FileNotFoundError:
-        print(f"File not found: {filepath}")
-        sys.exit()
 
 
 # Exit if no files were provided
@@ -36,3 +64,9 @@ if file_count == 0:
     print('Please add at least one file.')
     sys.exit()
 
+
+df = load_csv(args.imdb_tv)
+
+imdb_tv = standardise_columns(df, 'imdb_tv')
+# print(imdb_tv)
+print(imdb_tv[['type']])
